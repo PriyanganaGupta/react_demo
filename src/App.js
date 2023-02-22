0e92402786784ef0599f325d500188e64178b243
PriyanganaGupta <priyanganagupta04@gmail.com>
2023-02-22 21:26:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
+import ProductList from './components/ProductList';
+
 
 function App() {
+
+  const product = [
+    {
+      price:9999,
+      name: "Iphone 10SMax",
+      quantity : 0,
+    },
+    {
+      price:9999,
+      name: "Redmi Note 10SMax",
+      quantity : 0,
+    }
+  ]
+  
+  let[productList, setProductList] = useState(product)
+
+  const incrementQuantity = (index) => {
+    let newProductList = [...product]
+    newProductList[index].quantity++
+    setProductList(newProductList);
+  }
+
+  const decrementQuantity = (index) => {
+    let newProductList = [...productList]
+    newProductList[index].quantity--
+    setProductList(newProductList);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <NavBar/> 
+    <main className='container mt-5'>
+    <ProductList product ={product} incrementQuantity={incrementQuantity} decrementQuantity ={decrementQuantity}/>
+    </main>
+    
+  
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class NavBar extends React.Component {
+    render() { 
+        return ( 
+        <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Portfolio</a>
+        </div>
+      </nav>
+      );
+    }
+}
+ 
+export default NavBar;
